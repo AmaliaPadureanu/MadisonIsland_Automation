348a854de96e59adf3b96f682f66e5ebd0401928
AmaliaPadureanu <79747055+AmaliaPadureanu@users.noreply.github.com>
2023-01-10 18:24:09 +0200

    added a constructor and a checkErr() method to the EditAddressPage

--- src/test/java/Pages/EditAddressPage.java
+++ src/test/java/Pages/EditAddressPage.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class EditAddressPage extends BasePage {
 
+    WebDriverWait wait;
     @FindBy(how = How.ID, using = "firstname")
     private WebElement firstNameInput;
     @FindBy(how = How.ID, using = "middlename")
@@ -37,22 +40,24 @@ public class EditAddressPage extends BasePage {
     @FindBy(how = How.XPATH, using = "//*[@id=\"form-validate\"]/div[3]/button")
     private WebElement saveAddressButton;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[2]")
-    private WebElement firstNameWarning;
+    private WebElement firstNameError;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[4]")
-    private WebElement lastNameWarning;
+    private WebElement lastNameError;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[6]")
-    private WebElement telephoneWarning;
+    private WebElement telephoneError;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[8]")
-    private WebElement address1Warning;
+    private WebElement address1Error;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[10]")
-    private WebElement cityWarning;
+    private WebElement cityError;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[11]")
-    private WebElement stateWarning;
+    private WebElement stateError;
     @FindBy(how = How.XPATH, using = "(//div[@class='input-box'])[12]")
-    private WebElement zipCodeWarning;
+    private WebElement zipCodeError;
 
     public EditAddressPage(WebDriver driver) {
         super(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
     public void editContactInformation(String firstName, String middleName, String lastName, String company, String telephone, String fax) {
@@ -65,42 +70,57 @@ public class EditAddressPage extends BasePage {
         saveAddressButton.click();
     }
 
-    private String capitalizeFirstLetterOfEachWord(final String words) {
-        return Stream.of(words.trim().split("\\s"))
-                .filter(word -> word.length() > 0)
-                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
-                .collect(Collectors.joining(" "));
-    }
-
     public void editAddress(String address1, String address2, String city, String state, String zipCode, String country) {
         clearAndSendKeys(address1Input, address1);
         clearAndSendKeys(address2Input, address2);
         clearAndSendKeys(cityInput, city);
-        stateDropdown.selectByVisibleText(capitalizeFirstLetterOfEachWord(state));
+        stateDropdown.selectByVisibleText(state);
         clearAndSendKeys(zipCodeInput, zipCode);
-        countryDropdown.selectByVisibleText(capitalizeFirstLetterOfEachWord(country));
+        countryDropdown.selectByVisibleText(country);
     }
 
-    public String verifyFirstNameWarningMessage() {
-        if (firstNameWarning.isDisplayed()) {
-            return firstNameWarning.getText();
+    public boolean checkError(String expectedError, String errorType) {
+        if(errorType.equalsIgnoreCase("firstNameError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(firstNameError));
+                return expectedError.equalsIgnoreCase(firstNameError.getText());
+            } else return true;
+        } else if (errorType.equalsIgnoreCase("lastNameError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(lastNameError));
+                return expectedError.equalsIgnoreCase(lastNameError.getText());
+            } else return true;
+        } else if (errorType.equalsIgnoreCase("telephoneError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(telephoneError));
+                return expectedError.equalsIgnoreCase(telephoneError.getText());
+            }
+            else return true;
+        } else if (errorType.equalsIgnoreCase("address1Error")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(address1Error));
+                return expectedError.equalsIgnoreCase(address1Error.getText());
+            }
+            else return true;
+        } else if (errorType.equalsIgnoreCase("cityError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(cityError));
+                return expectedError.equalsIgnoreCase(cityError.getText());
+            }
+            else return true;
+        } else if (errorType.equalsIgnoreCase("stateError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(stateError));
+                return expectedError.equalsIgnoreCase(stateError.getText());
+            }
+            else return true;
+        } else if (errorType.equalsIgnoreCase("zipCodeError")) {
+            if (expectedError.length() > 0) {
+                wait.until(ExpectedConditions.visibilityOf(zipCodeError));
+                return expectedError.equalsIgnoreCase(zipCodeError.getText());
+            }
+            else return true;
         }
-        return "";
+        return false;
     }
-
-    public String verifyLastNameWarningMessage() {
-        if (lastNameWarning.isDisplayed()) {
-            return lastNameWarning.getText();
-        }
-        return "";
-    }
-
-    public String verifyTelephoneWarningMessage() {
-        if (telephoneWarning.isDisplayed()) {
-            return telephoneWarning.getText();
-        }
-        return "";
-    }
-
-
 }
