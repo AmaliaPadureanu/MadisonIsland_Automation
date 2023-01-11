c5af4de44a2b7cb67864700b77ed0535b935e5e6
AmaliaPadureanu <79747055+AmaliaPadureanu@users.noreply.github.com>
2023-01-11 14:42:55 +0200

    added searchAndSortTest to SearchTests class

--- src/test/java/Pages/SearchResultsPage.java
+++ src/test/java/Pages/SearchResultsPage.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SearchResultsPage extends BasePage {
 
@@ -15,6 +17,12 @@ public class SearchResultsPage extends BasePage {
 
     @FindBy(how = How.XPATH, using = "//*[@id=\"top\"]/body/div/div[2]/div[2]/div/div[2]/div[1]/p")
     private WebElement searchResultsNote;
+
+    @FindBy(how = How.XPATH, using = "//h2[@class='product-name']")
+    private List<WebElement> searchItemsNames;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"top\"]/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/select")
+    private WebElement sortBySelect;
 
     public SearchResultsPage(WebDriver driver) {
         super(driver);
@@ -24,5 +32,18 @@ public class SearchResultsPage extends BasePage {
 
     public String getSearchResultsNote() {
         return searchResultsNote.getText();
+    }
+
+    public void sortBy(String option) {
+        Select sortBy = new Select(sortBySelect);
+        sortBy.selectByVisibleText(option);
+    }
+
+    public LinkedList<String> getSearchResultsTitles() {
+        LinkedList<String> searchResultsTitles = new LinkedList<>();
+        for (WebElement item : searchItemsNames) {
+            searchResultsTitles.add(item.getText());
+        }
+        return searchResultsTitles;
     }
 }
--- src/test/java/Tests/SearchTests.java
+++ src/test/java/Tests/SearchTests.java
@@ -1,14 +1,21 @@
 package Tests;
 
 import Pages.SearchPage;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.checkerframework.checker.index.qual.PolyUpperBound;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SearchTests extends BaseTest {
 
-    @DataProvider(name = "validSearchDP")
+    @DataProvider(name = "basicValidSearchDP")
     public Object[][] validSearchDP() {
         return new Object[][] {
             {"dress"},
@@ -18,7 +25,7 @@ public class SearchTests extends BaseTest {
         };
     }
 
-    @DataProvider(name = "invalidSearchDP")
+    @DataProvider(name = "basicInvalidSearchDP")
     public Object[][] invalidSearchDP() {
         return new Object[][] {
                 {"asdfhjhdgsf", "Your search returns no results."},
@@ -28,18 +35,42 @@ public class SearchTests extends BaseTest {
         };
     }
 
-    @Test (dataProvider = "validSearchDP")
-    public void validSearchTest(String product) {
+    @DataProvider(name = "searchAndSortDp")
+    public Object[][] searchAndSortDP() {
+        return new Object[][] {
+                {"dress", "Name"},
+                {"pants", "Name"},
+                {"blouse", "Name"},
+//                {"#@SAFfFS435", "Your search returns no results."}
+        };
+    }
+
+    @Test (dataProvider = "basicValidSearchDP")
+    public void basicValidSearchTest(String product) {
         searchPage = PageFactory.initElements(driver, SearchPage.class);
         searchResultsPage = searchPage.search(product);
         Assert.assertTrue(searchResultsPage.getPageTitle().contains(product));
     }
 
-    @Test (dataProvider = "invalidSearchDP")
-    public void invalidSearchTest(String product, String searchResultsNote) {
+    @Test (dataProvider = "basicInvalidSearchDP")
+    public void basicInvalidSearchTest(String product, String searchResultsNote) {
         searchPage = PageFactory.initElements(driver, SearchPage.class);
         searchResultsPage = searchPage.search(product);
         Assert.assertTrue(searchResultsPage.getPageTitle().contains(product));
         Assert.assertEquals(searchResultsPage.getSearchResultsNote(), searchResultsNote);
+    }
+
+    @Test (dataProvider = "searchAndSortDp")
+    public void searchAndSortTest(String product, String criteria) {
+        searchPage = PageFactory.initElements(driver, SearchPage.class);
+        searchResultsPage = searchPage.search(product);
+        List<String> sortedAlphabetically = sortInAlphabeticalOrder(searchResultsPage.getSearchResultsTitles());
+        searchResultsPage.sortBy(criteria);
+        Assert.assertTrue(searchResultsPage.getPageTitle().contains(product));
+        Assert.assertEquals(searchResultsPage.getSearchResultsTitles(), sortedAlphabetically);
+    }
+
+    private List<String> sortInAlphabeticalOrder (List<String> list) {
+       return list.stream().sorted().collect(Collectors.toList());
     }
 }
